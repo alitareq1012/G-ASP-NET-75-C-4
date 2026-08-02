9c581b07d7cbd02a1e14abb6532565677eab003b
alitareq1012 <tali24593@gmail.com>
2026-08-02 16:09:02 +0300

    Question09

--- FirstApp3/Program.cs
+++ FirstApp3/Program.cs
@@ -53,6 +53,19 @@
 
             ///Console.WriteLine(prices.Length);
             #endregion
+
+            #region Question09
+            //double price;
+
+            //if (TryGetPrice("Clean Code", out price))
+            //{
+            //    Console.WriteLine(price);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Book not found");
+            //}
+            #endregion
         }
         #region Ouestion03
         ///  static void PrintWelcomeMessage()
@@ -94,6 +107,23 @@
         ///{
         ///    prices = new double[] { 10.0, 12.5, 15.0 };
         ///}
+        #endregion
+
+
+        #region Question09
+        //static bool TryGetPrice(string title, out double price)
+        //{
+        //    if (title == "Clean Code")
+        //    {
+        //        price = 25.5;
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        price = 0;
+        //        return false;
+        //    }
+        //}
         #endregion
     }
 }
